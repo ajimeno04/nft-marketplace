25dcd619e04ade904f86bc3ccb885b2a94bd5dd0
ajimeno04 <jimenobeltran11@gmail.com>
2025-02-13 17:14:05 +0100

    Add MongoDB integration with Mongoose
    - Added Mongoose and NestJS Mongoose module dependencies
    - Configured MongoDB connection using environment variable
    - Imported dotenv to load database connection URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI as string)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
